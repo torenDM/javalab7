59adcee8737bf2089bba16cdac66feff806e64e3
Matvey <demidovmatvey@rambler.ru>
2025-01-16 14:11:22 +0500

    Добавление отображение ошибки

--- src/main/java/com/example/energydrinks/controller/HomeController.java
+++ src/main/java/com/example/energydrinks/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/error")
+    public String error() {
+        return "error";
+    }
 }
